ce7c20a5b2012cf7e656c82ffd27f41428556bbb
Rutha <70570030+roseheart13@users.noreply.github.com>
2020-09-02 20:35:27 +0200

    idk

--- Weekly1Task1/Weekly1Task1.cpp
+++ Weekly1Task1/Weekly1Task1.cpp
@@ -5,14 +5,21 @@
 
 int name;
 int age;
-
+int number;
+int birthday;
 
 int main()
 {
     std::cout << "What is your full name: ";
-        std::cin >> name;
+    getline( cin, name);
+        //std::cin >> name;
     std::cout << "What is your age: ";
         std::cin >> age;
-
+    std::cout << "What is your phone number: ";
+        std::cin >> number;
+    std::cout << "When is your birthday: ";
+        std::cin >> birthday;
+    
+        std::cout << "Name - " << name << std:: endl;
 
 }
