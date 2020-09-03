8f92b3b6ab7f9a1907dbe06cec5239565e770ef2
Rutha <70570030+roseheart13@users.noreply.github.com>
2020-09-03 17:15:13 +0200

    Not done

--- Weekly1Task1/Weekly1Task1.cpp
+++ Weekly1Task1/Weekly1Task1.cpp
@@ -3,23 +3,34 @@
 #include <iostream>
 #include <string>
 
-int name;
+#define MAX_LENGTH 100
+
 int age;
 int number;
-int birthday;
+//int birthday;
 
 int main()
 {
+    char name[MAX_LENGTH] = { 0 };
+    char birthday[MAX_LENGTH] = { 0 };
+
     std::cout << "What is your full name: ";
-    getline( cin, name);
-        //std::cin >> name;
+    std::cin.getline(name, MAX_LENGTH);
     std::cout << "What is your age: ";
         std::cin >> age;
     std::cout << "What is your phone number: ";
         std::cin >> number;
-    std::cout << "When is your birthday: ";
-        std::cin >> birthday;
+    std::cout << "When is your birthday dd/mm/yyyy: ";
+   // std::cin >> day >> month >> year;
+        std::cin.getline(birthday, MAX_LENGTH);
+    //std::cin >> birthday;
     
         std::cout << "Name - " << name << std:: endl;
+        std::cout << "Age - " << age << std::endl;
+        std::cout << "Number - " << number << std::endl;
+       // std::cout << "Birthday - " << day << month << year << std::endl;
+        std::cout << "Birthday - " << birthday << std::endl;
+
+
 
 }
