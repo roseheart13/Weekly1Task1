0f41bfb9ee3da3cbd919264efaf0b1561cfb1a08
Rutha <70570030+roseheart13@users.noreply.github.com>
2020-09-06 15:33:13 +0200

    Done

--- Weekly1Task1/Weekly1Task1.cpp
+++ Weekly1Task1/Weekly1Task1.cpp
@@ -1,35 +1,43 @@
 
-
 #include <iostream>
 #include <string>
 
 #define MAX_LENGTH 100
 
+
+
+std::string s;
 int age;
 int number;
-//int birthday;
+int day;
+int month;
+int year;
 
 int main()
 {
-    char name[MAX_LENGTH] = { 0 };
-    char birthday[MAX_LENGTH] = { 0 };
+    char fullname[MAX_LENGTH] = { 0 };
+    
 
     std::cout << "What is your full name: ";
-    std::cin.getline(name, MAX_LENGTH);
+    std::cin.getline(fullname, MAX_LENGTH);
+    
     std::cout << "What is your age: ";
         std::cin >> age;
     std::cout << "What is your phone number: ";
         std::cin >> number;
-    std::cout << "When is your birthday dd/mm/yyyy: ";
-   // std::cin >> day >> month >> year;
-        std::cin.getline(birthday, MAX_LENGTH);
-    //std::cin >> birthday;
+    std::cout << "When is your birthday dd/mm/yyyy: " << std::endl;
+    std::cout << "Day: ";
+    std::cin >> day;
+    std::cout << "Month: ";
+    std::cin >> month;
+    std::cout << "Year: ";
+    std::cin >> year;
     
-        std::cout << "Name - " << name << std:: endl;
+
+        std::cout << "Name - " << fullname << std:: endl;
         std::cout << "Age - " << age << std::endl;
         std::cout << "Number - " << number << std::endl;
-       // std::cout << "Birthday - " << day << month << year << std::endl;
-        std::cout << "Birthday - " << birthday << std::endl;
+        std::cout << "Birthday - " << day << "/" << month << "/" << year << std::endl;
 
 
 
